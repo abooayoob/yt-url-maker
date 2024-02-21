b64f43b1a0c384b758282d97b891b19a26e311f1
Mohammad Ali Khan <brorentilhammad@gmail.com>
2024-02-21 16:51:17 +0100

    stylez

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { useMachine } from "@xstate/react";
 import { ytUrlMachine } from "./yt-url-machine";
 import React from "react";
+import { Button, Form, Input, Label, TextField } from "react-aria-components";
+
 declare global {
   interface Window {
     player: YT.Player;
     onYouTubeIframeAPIReady: () => void;
-    theinput: HTMLInputElement;
   }
 }
 
@@ -86,62 +87,73 @@ function App() {
   }, []);
 
   return (
-    <>
-      <div id="player"></div>
-      {state.matches("Ready.Show url") &&
-        !state.matches("Ready.Show url.Show error") && (
-          <h2>{state.context.generatedUrl}</h2>
-        )}
-      {state.matches("Ready.Show url.Show error") && (
-        <h2 style={{ color: "red" }}>{state.context.error}</h2>
-      )}
-      {state.matches("Ready.Show url") && (
-        <button
-          style={{
-            backgroundColor: state.matches("Ready.Show url.Idle")
-              ? "white"
-              : state.matches("Ready.Show url.Error copying")
-              ? "red"
-              : state.matches("Ready.Show url.Success copying")
-              ? "green"
-              : "yellow",
-          }}
-          onClick={() => {
-            send({ type: "copy url" });
-          }}
-        >
-          Copy url
-        </button>
-      )}
-      {state.matches("Ready") && (
-        <div>
-          <form onSubmit={handleSubmit}>
-            <input type="text" name="theinput" onPaste={handlePaste} />
-          </form>
-          <button
-            onClick={() => {
-              send({
-                type: "set startTime",
-                payload: { startTime: window.player.getCurrentTime() },
-              });
-            }}
-          >
-            start time
-          </button>
-          <button
-            onClick={() =>
-              send({
-                type: "set endTime",
-                payload: { endTime: window.player.getCurrentTime() },
-              })
-            }
-          >
-            end time
-          </button>
+    <main className="h-full min-h-screen w-full min-w-full bg-slate-100 dark:bg-slate-800 dark:text-slate-300">
+      <div className="mx-auto mt-0 grid max-w-3xl grid-cols-1 grid-rows-[2fr_1fr]">
+        <div id="player" className="aspect-video h-[60vh] w-full"></div>
+
+        <div className="flex flex-col gap-4 p-4">
+          {state.matches("Ready") && (
+            <div className="flex flex-col items-start gap-4">
+              <Form onSubmit={handleSubmit}>
+                <TextField
+                  className="flex flex-col items-baseline gap-1"
+                  name="theinput"
+                >
+                  <Label className="text-sm">Url or Id</Label>
+                  <Input
+                    className="rounded-sm bg-slate-600 px-2 text-slate-200 outline-none focus-visible:ring-1 focus-visible:ring-slate-300"
+                    onPaste={handlePaste}
+                  />
+                </TextField>
+              </Form>
+              <div className="flex gap-2">
+                <Button
+                  className="rounded-sm  border-2 border-slate-300 px-2 py-0 outline-none focus-visible:ring-1 focus-visible:ring-slate-300 pressed:bg-slate-600"
+                  onPress={() => {
+                    send({
+                      type: "set startTime",
+                      payload: { startTime: window.player.getCurrentTime() },
+                    });
+                  }}
+                >
+                  start time
+                </Button>
+                <Button
+                  className="rounded-sm  border-2 border-slate-300 px-2 py-0 outline-none focus-visible:ring-1 focus-visible:ring-slate-300 pressed:bg-slate-600"
+                  onPress={() =>
+                    send({
+                      type: "set endTime",
+                      payload: { endTime: window.player.getCurrentTime() },
+                    })
+                  }
+                >
+                  end time
+                </Button>
+              </div>
+            </div>
+          )}
+          {state.matches("Ready.Show url") &&
+            !state.matches("Ready.Show url.Show error") && (
+              <h2>{state.context.generatedUrl}</h2>
+            )}
+          {state.matches("Ready.Show url.Show error") && (
+            <h2 className="text-red-600">{state.context.error}</h2>
+          )}
+          {state.matches("Ready.Show url") && (
+            <Button
+              className={`w-fit rounded-sm border-2 border-slate-300 px-2 py-0 outline-none focus-visible:ring-1 focus-visible:ring-slate-300 pressed:bg-slate-600 ${state.matches("Ready.Show url.Error copying") ? "bg-red-600" : state.matches("Ready.Show url.Success copying") ? "bg-green-600" : ""}`}
+              onPress={() => {
+                send({ type: "copy url" });
+              }}
+            >
+              Copy url
+            </Button>
+          )}
+
+          {/* <pre>{JSON.stringify(state.context, null, 2)}</pre> */}
         </div>
-      )}
-      {/* <pre>{JSON.stringify(state.context, null, 2)}</pre> */}
-    </>
+      </div>
+    </main>
   );
 }
 
